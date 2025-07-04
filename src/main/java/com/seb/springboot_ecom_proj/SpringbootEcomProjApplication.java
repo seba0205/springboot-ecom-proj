052a9b730d208d38c178f9083eb3b2839d93d0ea
seba0205 <83637951+seba0205@users.noreply.github.com>
2025-07-04 08:06:35 -0400

    Initialise git repo

---
+++ src/main/java/com/seb/springboot_ecom_proj/SpringbootEcomProjApplication.java
@@ -0,0 +1,13 @@
+package com.seb.springboot_ecom_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcomProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEcomProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seb/springboot_ecom_proj/SpringbootEcomProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb.springboot_ecom_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcomProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
