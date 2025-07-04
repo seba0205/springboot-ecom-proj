869c9cc98e22ef17cd1ad8337e3bb18f53c5c1b5
seba0205 <83637951+seba0205@users.noreply.github.com>
2025-07-04 09:18:57 -0400

    completed tut 24

--- src/main/java/com/seb/springboot_ecom_proj/controller/ProductController.java
+++ src/main/java/com/seb/springboot_ecom_proj/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.seb.springboot_ecom_proj.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.seb.springboot_ecom_proj.model.Product;
 import com.seb.springboot_ecom_proj.service.ProductService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
