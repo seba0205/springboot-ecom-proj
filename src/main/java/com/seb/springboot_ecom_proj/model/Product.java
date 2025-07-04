055fddf6439fcec0fd6e1daeb8f9b340dfaf6d62
seba0205 <83637951+seba0205@users.noreply.github.com>
2025-07-04 09:05:27 -0400

    added db

--- src/main/java/com/seb/springboot_ecom_proj/model/Product.java
+++ src/main/java/com/seb/springboot_ecom_proj/model/Product.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String desc;
