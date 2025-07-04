fd58114ea0164f2e2542730aa8c218c8e5971d75
seba0205 <83637951+seba0205@users.noreply.github.com>
2025-07-04 08:50:14 -0400

    autowired fix

--- src/main/java/com/seb/springboot_ecom_proj/controller/ProductController.java
+++ src/main/java/com/seb/springboot_ecom_proj/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.seb.springboot_ecom_proj.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.seb.springboot_ecom_proj.service.ProductService;
 @RequestMapping("/api")
 public class ProductController {
 
+    @Autowired
     private ProductService service;
     @RequestMapping("/")
     public String greet(){
